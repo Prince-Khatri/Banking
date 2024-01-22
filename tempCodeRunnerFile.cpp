f8a6b4c18e88b5d5696d5691f7c330a6d9b4968f
Prince Khatri <151055926+Prince-Khatri@users.noreply.github.com>
2024-01-22 21:56:10 +0000

    "Commit da"

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,13 @@
-// printdown();
+void Display_Statement(){
+        string cp[Statement.size()+1];
+        for(size_t i=0;i<Statement.size();i++){
+            cp[i]=Statement[i];
+
+        }
+        ostringstream occ;
+        occ<<"Amount left "<<setprecision('0')<<Amount;
+        cp[Statement.size()]=occ.str();
+
+        print(cp,Statement.size()+1);
+
+    }
